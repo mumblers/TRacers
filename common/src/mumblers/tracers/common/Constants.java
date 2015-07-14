4801447368a17691cf8a1ad06ff652a27382e5df
sinius15 <sinius15@gmail.com>
2015-07-14 11:38:59 +0200

    adding common module and creating constants class

---
+++ common/src/mumblers/tracers/common/Constants.java
@@ -0,0 +1,12 @@
+package mumblers.tracers.common;
+
+/**
+ * Created by Sinius15 on 14-7-2015.
+ * Constants used by the client and server
+ */
+public class Constants {
+
+    public static final String VERSION = "0.01";
+    public static final int SERVER_PORT = 1862;
+
+}
